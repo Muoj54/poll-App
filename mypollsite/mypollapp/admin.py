582e865e4c9205f9634cbcd95265c7ffbab98367
Muoj54 <160734738+Muoj54@users.noreply.github.com>
2024-06-10 17:17:36 +0300

    Add files via upload

--- mypollsite/mypollapp/admin.py
+++ mypollsite/mypollapp/admin.py
@@ -1,5 +1,29 @@
+from django.utils import timezone
 from django.contrib import admin
-from .models import Question  
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4  # Number of extra choice fields to display
+
+class QuestionAdmin(admin.ModelAdmin):
+    
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    def was_published_recently(self, obj):
+        now = timezone.now()
+        return obj.pub_date <= now <= now - timezone.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 # Register your models here.
